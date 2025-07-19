f2b7c715daf2c23d3c2fd2fd497434b95dca468c
Sariaki <klmncsgo@gmail.com>
2025-07-19 19:17:20 +0200

    Change includes

--- obfuscator-pass/src/obfuscator.cpp
+++ obfuscator-pass/src/obfuscator.cpp
@@ -1,12 +1,12 @@
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/IntrinsicInst.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IR/Constants.h"
-#include "llvm/Pass.h"
-#include "llvm/IR/PassManager.h"
-#include "llvm/Passes/PassBuilder.h"
-#include "llvm/Passes/PassPlugin.h"
-#include "llvm/Support/raw_ostream.h"
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/IntrinsicInst.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Constants.h>
+#include <llvm/Pass.h>
+#include <llvm/IR/PassManager.h>
+#include <llvm/Passes/PassBuilder.h>
+#include <llvm/Passes/PassPlugin.h>
+#include <llvm/Support/raw_ostream.h>
 #include <iostream>
 //#include "GenerateDistribution.hpp"
 
